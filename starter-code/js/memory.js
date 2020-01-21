402b32f08d8782ac8cef02fd56b7248bba2f19c2
Nelson <nelsoncc1812@gmail.com>
2020-01-21 20:26:08 +0000

    First js done exept the f... randomizer function :3

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,44 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
+  constructor(cards) {
+    this.cards = cards
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+    this.pairs=cards.length/2
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+
+    shuffleCards(array) {
+
+      if (!array) return undefined
+      else {
+    let m = array.length
+     let t
+     let i
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+    }
+  }
+
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if (card1 == card2) {
+      this.pairsGuessed++
+      return true
+    }
+    return false
+  }
+
+
+isFinished() {
+return this.pairs==this.pairsGuessed? true: false
+
+}
 }
